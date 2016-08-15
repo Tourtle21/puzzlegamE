b07ad98135c9d15c6487998bf2062d7544a087f7
LOGAN HIRSCHI <logan.d@hotmail.com>
2016-08-15 00:47:10 -0600

    Randomized spawn
    I added a random spawn and I started a move function

--- src/components/board.js
+++ src/components/board.js
@@ -5,7 +5,20 @@ var Board = React.createClass({
     var createBoard = function (number) {
       return (<Squares key={number} inner={number} />)
     }
-    var list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var arry = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var cool = function(arry) {
+      var currentIndex = arry.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = arry[currentIndex];
+        arry[currentIndex] = arry[randomIndex];
+        arry[randomIndex] = temporaryValue
+      }
+      return(arry);
+    }
+    cool(arry);
+    var list = arry;
     var squares = list.map(createBoard, this)
     return (
       <div id="board">
---
+++ src/components/move.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Squares from "./squares"
+var Move = React.createClass( {
+	render: function () {
+		return (
+		
+			)
+	}
+})
